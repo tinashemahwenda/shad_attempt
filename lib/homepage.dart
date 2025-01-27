18d3bb97db9a839f7531ae9bae136afc9ba7116a
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-27 15:34:42 +0200

    Added some padding, cool, its working!

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,15 +7,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Spacer(),
-          Text(
-            'Hello ShadCN UI',
-            style: ShadTheme.of(context).textTheme.h1Large,
-          ),
-          Spacer()
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20),
+            Text(
+              'Hello ShadCN UI h1',
+              style: ShadTheme.of(context).textTheme.h1,
+            ),
+            Text(
+              'Hello ShadCN UI p',
+              style: ShadTheme.of(context).textTheme.p,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Alerts',
+              style: ShadTheme.of(context).textTheme.h4,
+            ),
+            SizedBox(height: 10),
+            ShadAlert(
+              iconColor: Colors.green,
+              decoration: ShadDecoration(
+                color: Colors.green[100],
+              ),
+              iconData: LucideIcons.check,
+              title: Text('Well done'),
+              description:
+                  Text('You have successfully made your first ShadCN/Ui Alert'),
+            ),
+            ShadAlert.destructive(
+              iconData: LucideIcons.circleAlert,
+              title: Text('Error'),
+              description:
+                  Text('You have entered the wrong login details. Try again'),
+            )
+          ],
+        ),
       ),
     );
   }
