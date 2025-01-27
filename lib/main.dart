751d6b7cd8844afa1da4eb5490b47ffe643b5c6b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-27 15:03:56 +0200

    Made a homepage for a simple view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shad_attempt/homepage.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 void main() {
@@ -10,6 +11,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ShadApp();
+    return ShadApp.material(
+      home: HomePage(),
+    );
   }
 }
